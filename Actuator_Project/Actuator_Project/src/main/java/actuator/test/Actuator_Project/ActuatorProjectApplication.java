9444a3b3d9f9f17baea27e86dfdc10095c1405e7
Satyendra kumar <Satyendrakumar@bhavnacorp.com>
2018-06-07 11:42:17 +0530

    Actuator

---
+++ Actuator_Project/Actuator_Project/src/main/java/actuator/test/Actuator_Project/ActuatorProjectApplication.java
@@ -0,0 +1,12 @@
+package actuator.test.Actuator_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorProjectApplication.class, args);
+	}
+}
